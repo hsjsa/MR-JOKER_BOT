0254b1f337a60a4aeca317cad211d6a255e01084
hsjsa <101786584+hsjsa@users.noreply.github.com>
2022-03-27 09:46:24 +0530

    Update and rename sample_config.py to config.py

--- mrjoker/config.py
+++ mrjoker/config.py
@@ -14,24 +14,24 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID =   # integer value, dont use ""
-    API_HASH = ""
-    BOT_ID = ""
-    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1833279646  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "kavinduaj"
-    SUDO_USERS = "1833279646"
-    SUPPORT_USERS = "1833279646"
-    SUPPORT_CHAT = "lkhitech"  # Your own group for support, do not add the @
+    API_ID = 6235351  # integer value, dont use ""
+    API_HASH = "f68fde1378da8f85a243f2ae57f2fcf9"
+    BOT_ID = "5031427097:AAFBwYpQ3wlgXrv4nSDPf8J3Mcflh3ZJ9KU"
+    TOKEN = "5031427097:AAFBwYpQ3wlgXrv4nSDPf8J3Mcflh3ZJ9KU"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 5042525177  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "IamRooot"
+    SUDO_USERS = "1818001569 2005755862 1287276743 5042525177"
+    SUPPORT_USERS = "1818001569 2005755862 1287276743 5042525177"
+    SUPPORT_CHAT = "Mr_Jokr_support"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001237555668
+        -1001235511467
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001237555668
+        -1001235511467
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = ""  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = "postgres://renjtfrp:aOZT5-2R3Lp7tHI-bQ_krH9ZlOLhUgaj@arjuna.db.elephantsql.com/renjtfrp"  # needed for any database modules
     REDIS_URI = " "
     LOAD = []
     NO_LOAD = []
@@ -40,8 +40,8 @@ class Config(object):
     URL = None
     HEROKU_API_KEY = ""
     HEROKU_APP_NAME = ""
-    BOT_USERNAME = ""
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    BOT_USERNAME = "Mr_Jkr_Bot"
+    SPAMWATCH_API = "yfJmdR0rNj1MclUWjiTblobi3QRynjNBqQEjNn~nb3kQY6hvmQbgoH4O0encL1QA"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
     ARQ_API_KEY = "GBVANC-POIBJH-XAYVHT-XZBPTG-ARQ"
     ARQ_API_URL = "https://thearq.tech/"
@@ -75,9 +75,9 @@ class Config(object):
     BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
     CASH_API_KEY = (
-        "xyz"  # Get your API key from https://www.alphavantage.co/support/#api-key
+        "Y0L02LBR4WLNIK5W"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = "xyz"  # Get your API key from https://timezonedb.com/api
+    TIME_API_KEY = "6DEOJMP7M03L"  # Get your API key from https://timezonedb.com/api
     WALL_API = (
         "xyz"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
     )
@@ -86,7 +86,7 @@ class Config(object):
     SPAMMERS = None
     REM_BG_API_KEY = ""
     GENIUS_API_TOKEN = ""
-    MONGO_DB = ""
+    MONGO_DB = "mongodb+srv://root:root@cluster0.nuzmg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     
 
 class Production(Config):
